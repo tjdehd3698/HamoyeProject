db8141470f8de28ebab186f6cd79b40a4b845ccb
tjdehd3698 <64083411+tjdehd3698@users.noreply.github.com>
2022-08-30 19:32:14 +0900

    a

--- HamoyeProject/src/main/java/com/bnk/hamoye/controller/UserController.java
+++ HamoyeProject/src/main/java/com/bnk/hamoye/controller/UserController.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 import javax.servlet.http.HttpSession;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,29 +25,30 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
 	private final UserService userService;
 	
+	@PostMapping("register.do")
+	public String registerUser(User user, Model model) {
+		try {
+			userService.registerUser(user);
+			return "MWPCMNV02M";
+		} catch (SQLException e) {
+			System.out.println("registerUser 에러 : "+ e.getMessage());
+			return "";
+		}
+	}
 	
-//	@PostMapping("")
-//	public String registerUser(User user, Model model) {
-//		try {
-//			userService.registerUser(user);
-//		} catch (SQLException e) {
-//			System.out.println("registerUser 에러 : "+ e.getMessage());
-//		}
-//		return "";
-//	}
-	
-//	@GetMapping()
-//	@ResponseBody
-//	public String duplicateId(@RequestParam User user,Model model) {
-//		boolean result;
-//		try {
-//			result = userService.duplicateId(user);
-//			model.addAttribute("result", result);
-//		} catch (SQLException e) {
-//			System.out.println("duplicateId 에러 : "+ e.getMessage());
-//		}
-//		return "";
-//	}
+	@PostMapping("duplicateId.do")
+	@ResponseBody
+	public String duplicateId( User user,Model model) {
+		boolean result;
+		try {
+			result = userService.duplicateId(user);
+			if(result) return "F";
+			else return "T";
+		} catch (SQLException e) {
+			System.out.println("duplicateId 에러 : "+ e.getMessage());
+		}
+		return "";
+	}
 	
 	@PostMapping("login.do")
 	@ResponseBody
@@ -69,12 +70,21 @@ public class UserController {
 		return "";
 	}
 	
-	@RequestMapping("logout.do")
+	@PostMapping("logout.do")
 	public String logout(HttpSession session) {
 		session.invalidate();
 		return "index";
 	}
 	
+//	@PostMapping()
+//	public String joinAccount(Account account,String ecoChallengeId, HttpSession session) {
+//		try {
+//			userService.joinAccount(account, (String)session.getAttribute("user"), ecoChallengeId);
+//		} catch (SQLException e) { 
+//			System.out.println("joinAccount 에러 : "+ e.getMessage());
+//		}
+//		return "";
+//	}
 //	
 //	@GetMapping()
 //	public String findPassword(User user, Model model) {
@@ -87,34 +97,80 @@ public class UserController {
 //		}
 //		return "";
 //	}
+//	
+//	@PostMapping()
+//	public String updateUser(User user) {
+//		try {
+//			userService.updateUser(user);
+//		} catch (SQLException e) {
+//			System.out.println("updateUser 에러 : "+ e.getMessage());
+//		}
+//		return "";
+//	}
+//	
+//	@GetMapping()
+//	public String getUserInfo(Model model, HttpSession session) {
+//		try {
+//			User user = userService.getUserInfo((String)session.getAttribute("user"));
+//			model.addAttribute("result", user);
+//		} catch (SQLException e) {
+//			System.out.println("getUserInfo 에러 : "+ e.getMessage());
+//		}
+//		return "";
+//	}
+//	
+//	@PostMapping()
+//	public String withdraw(HttpSession session) {
+//		try {
+//			userService.withdrawUser((String)session.getAttribute("user"));
+//		} catch (SQLException e) {
+//			System.out.println("withdraw 에러 : "+ e.getMessage());
+//		}
+//		return "";
+//	}
 	
 //	@GetMapping()
-//	public String checkEcoChallenge(Model model,HttpSession session) {
+//	public String checkEcoChallenge(HttpSession session, Model model) {
 //		try {
 //			String findId = userService.checkEcoChallenge((String)session.getAttribute("user"));
-//			if(findId == null) {
-//				model.addAttribute("result", "T");
-//			}
-//			else {
-//				model.addAttribute("result", "F");
-//			}
+//
 //		} catch (SQLException e) {
 //			System.out.println("checkEcoChallenge 에러 : "+ e.getMessage());
 //		}
 //		return "";
 //	}
 	
+	@GetMapping()
+	public String checkEcoChallenge(Model model,HttpSession session) {
+		return "";
+	}
 	
+//	
 //	@PostMapping()
-//	public String joinAccount(Account account, HttpSession session, String ecoChallengeId ) {
-//		try {
-//			userService.joinAccount(account, (String)session.getAttribute("user"), ecoChallengeId);
-//		} catch (SQLException e) {
-//			System.out.println("joinAccount 에러 : "+ e.getMessage());
-//		}
+//	public String joinAccount(Account account, String userId, String ecoChallengeId) {
+//		
 //		return "";
 //	}
 	
-	
+	@PostMapping("")
+	@ResponseBody
+	public String adminLogin(User user, HttpSession session) {
+		try {
+			
+			User findUser = userService.login(user);
+			
+			if(findUser!=null){
+				session.setAttribute("user", findUser.getUserId());
+				session.setAttribute("userName", findUser.getUserName());
+				System.out.println("로그인 성공");
+				return "T";
+			} 
+			else return "F";
+			
+		} catch (SQLException e) {
+			System.out.println("login 에러 : "+ e.getMessage());
+		}
+		return "";
+	}
 	
 }

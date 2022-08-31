987f4568558c0603e240ddb04d11eeb3ce7bf11c
tjdehd3698 <64083411+tjdehd3698@users.noreply.github.com>
2022-08-31 20:15:54 +0900

    import추가

--- HamoyeProject/src/main/java/com/bnk/hamoye/controller/AdminController.java
+++ HamoyeProject/src/main/java/com/bnk/hamoye/controller/AdminController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bnk.hamoye.domain.Status;
+import com.bnk.hamoye.domain.TripStatus;
 import com.bnk.hamoye.domain.User;
 import com.bnk.hamoye.service.AdminService;
 

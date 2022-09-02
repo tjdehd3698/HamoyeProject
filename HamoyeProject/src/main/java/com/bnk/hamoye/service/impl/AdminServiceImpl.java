04b9aba6dabd7d862c503a160ccb92277f81ef65
tjdehd3698 <64083411+tjdehd3698@users.noreply.github.com>
2022-09-02 16:19:20 +0900

    admin 수정

--- HamoyeProject/src/main/java/com/bnk/hamoye/service/impl/AdminServiceImpl.java
+++ HamoyeProject/src/main/java/com/bnk/hamoye/service/impl/AdminServiceImpl.java
@@ -205,15 +205,15 @@ public class AdminServiceImpl implements AdminService{
 	}
 
 	@Override
-	public Map<String, Integer> getUserCntByEcoChallengeType(int totalUserCnt) throws Exception {
-		Map<String,Integer> result = new HashMap<String, Integer>();
-		int rest = totalUserCnt; //아무 에코 챌린지에 참여안한 사람 수
+	public Map<String, Double> getUserCntByEcoChallengeType(int totalUserCnt) throws Exception {
+		Map<String,Double> result = new HashMap<String, Double>();
+		double rest = totalUserCnt; //아무 에코 챌린지에 참여안한 사람 수
 		
 		List<String> typeList = ecoChallengeDAO.getEcoChallengeType();
 		for(String type : typeList) {
 			List<User> userList = userDAO.getUserByEcoChallengeType(type);
 			rest-=userList.size();
-			result.put(type, userList.size());
+			result.put(type, (double)userList.size()/(double)totalUserCnt*100.0);
 		}
 		
 		result.put("기타", rest);

e7f39821209e29fb36f2d098c1b81c180839e5dd
tjdehd3698 <64083411+tjdehd3698@users.noreply.github.com>
2022-09-04 22:20:51 +0900

    수정

--- HamoyeProject/src/main/java/com/bnk/hamoye/service/impl/AdminServiceImpl.java
+++ HamoyeProject/src/main/java/com/bnk/hamoye/service/impl/AdminServiceImpl.java
@@ -255,6 +255,7 @@ public class AdminServiceImpl implements AdminService{
 	@Override
 	public int updateUserParticipationCountWithPublicTransportaion(String ecoChallengeId) throws Exception {
 		List<User> userList = userDAO.getUserByEcoChallenge(ecoChallengeId);
+		
 		for(User user : userList) {
 			List<Payment> paymentList = paymentDAO.getPublicTransportationUsageByUserId(user.getUserId());
 			
